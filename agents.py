1ac0375106e0c6f289f5c124371cd9d376330660
karthick ramakrishnan <magnumnighthawk@gmail.com>
2025-01-08 15:49:30 +0000

    Added Tavily search tool to agent example

--- agents.py
+++ agents.py
@@ -2,6 +2,7 @@ from langchain_community import tools
 import inspect
 from langchain_openai import ChatOpenAI
 from langchain_community.tools import YouTubeSearchTool
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import HumanMessage
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
@@ -42,11 +43,12 @@ def list_available_tools():
 
     
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.9)
-prompt = "Using reliable food science and knowledge of South indian cuisine, create the perfect protein rich breakfast for a 30 year old male looking to build muscle and lose weight. Suggest good youtube videos to watch for the recipe."
+prompt = "Using south indian cuisine, create the perfect protein rich breakfast for a 30 year old male looking to build muscle and lose weight. Suggest some recipes on Youtube and some good blog posts"
 
-tool = YouTubeSearchTool()
+youtube_search = YouTubeSearchTool()
+tavily_search = TavilySearchResults(max_results=2)
 
-agent = create_react_agent(llm, tools=[tool])
+agent = create_react_agent(llm, tools=[youtube_search, tavily_search])
 response = agent.invoke({"messages": [HumanMessage(content=prompt)]})
 
 print(response["messages"])
